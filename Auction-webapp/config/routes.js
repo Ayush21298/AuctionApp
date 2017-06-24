c52a515b2fe5a4d698859c6b0096930593125e13
Ayush <patel.ayush08@gmail.com>
2017-06-25 04:00:10 +0530

    Templates linking

--- Auction-webapp/config/routes.js
+++ Auction-webapp/config/routes.js
@@ -35,9 +35,78 @@ module.exports.routes = {
   '/': {
     view: 'custom/index'
   },
-  '/about':{
+  '/index.html': {
+    view: 'custom/'
+  },
+  '/card.html': {
+    view: 'custom/card'
+  },
+  '/contact.html': {
+    view: 'custom/contact'
+  },
+  '/faq.html': {
+    view: 'custom/faq'
+  },
+  '/help.html': {
+    view: 'custom/help'
+  },
+  '/login.html': {
+    view: 'custom/login'
+  },
+  '/marketplace.html': {
+    view: 'custom/marketplace'
+  },
+  '/offers.html': {
+    view: 'custom/offers'
+  },
+  '/privacy.html': {
+    view: 'custom/privacy'
+  },
+  '/products.html': {
+    view: 'custom/products'
+  },
+  '/product1.html': {
+    view: 'custom/product1'
+  },
+  '/product2.html': {
+    view: 'custom/product2'
+  },
+  '/product3.html': {
+    view: 'custom/product3'
+  },
+  '/product4.html': {
+    view: 'custom/product4'
+  },
+  '/product5.html': {
+    view: 'custom/product5'
+  },
+  '/product6.html': {
+    view: 'custom/product6'
+  },
+  '/product7.html': {
+    view: 'custom/product7'
+  },
+  '/product8.html': {
+    view: 'custom/product8'
+  },
+  '/product9.html': {
+    view: 'custom/product9'
+  },
+  '/signup.html': {
+    view: 'custom/signup'
+  },
+  '/single.html': {
+    view: 'custom/single'
+  },
+  '/sitemap.html': {
+    view: 'custom/sitemap'
+  },
+  '/values.html': {
+    view: 'custom/values'
+  },
+  '/about.html':{
     view:'custom/about'
-  }
+  },
 
   /***************************************************************************
   *                                                                          *
