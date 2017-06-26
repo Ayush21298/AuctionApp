c4c088e252389352b9f9eeb72e60b990060761e0
Ayush <patel.ayush08@gmail.com>
2017-06-26 23:15:47 +0530

    user modal

--- Auction-webapp/api/models/User.js
+++ Auction-webapp/api/models/User.js
@@ -8,7 +8,18 @@
 module.exports = {
 
   attributes: {
-
+    username: {
+      type:'string',
+      required:true,
+      unique:true,
+    },
+    password: {
+      type:'string',
+      required:true,
+    },
+    email: {
+      type:'string',
+      required:true,
+    },
   }
 };
-
